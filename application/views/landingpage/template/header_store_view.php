02c8a6c9066dbeb777a9cfd28cab877fbf8be5b0
aldi naufal <ry.prg75@gmail.com>
2020-02-05 09:45:49 -0800

    Ekopz V2

--- application/views/landingpage/template/header_store_view.php
+++ application/views/landingpage/template/header_store_view.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Vegefoods - Free Bootstrap 4 Template by Colorlib</title>
+    <title>Ekopz | <?php echo $nama; ?></title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -38,20 +38,20 @@
 
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav ml-auto">
-            <li class="nav-item active"><a href="<?php echo base_url(); ?>landingpage/home" class="nav-link">Home</a></li>
+            <li class="nav-item active"><a href="<?php echo base_url(); ?>home" class="nav-link">Home</a></li>
 						<li class="nav-item dropdown">
 							<a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Ekopz Store</a>
 							<div class="dropdown-menu" aria-labelledby="dropdown04">
-								<a class="dropdown-item" href="<?php echo base_url(); ?>landingpage/store">Shop</a>
+								<a class="dropdown-item" href="<?php echo base_url(); ?>store">Shop</a>
 								<a class="dropdown-item" href="wishlist.html">Wishlist</a>
 								<a class="dropdown-item" href="product-single.html">Single Product</a>
 								<a class="dropdown-item" href="cart.html">Cart</a>
 								<a class="dropdown-item" href="checkout.html">Checkout</a>
 							</div>
 						</li>
-						<li class="nav-item"><a href="<?php echo base_url(); ?>landingpage/about" class="nav-link">About Us</a></li>
-						<li class="nav-item"><a href="<?php echo base_url(); ?>landingpage/koperasi" class="nav-link">Join Koperasi</a></li>
-						<li class="nav-item"><a href="<?php echo base_url(); ?>landingpage/contact" class="nav-link">Contact</a></li>
+						<li class="nav-item"><a href="<?php echo base_url(); ?>about" class="nav-link">About Us</a></li>
+						<li class="nav-item"><a href="<?php echo base_url(); ?>koperasi" class="nav-link">Join Koperasi</a></li>
+						<li class="nav-item"><a href="<?php echo base_url(); ?>contact" class="nav-link">Contact</a></li>
 	          <li class="nav-item cta cta-colored"><a href="cart.html" class="nav-link"><span class="icon-shopping_cart"></span>[0]</a></li>
 
 	        </ul>
